9dd76a2e9ef78144aa30f566fe2c780532d96181
GBKS <chri@sto.ph>
2020-10-07 08:55:05 +0200

    Cleaning up
    Let's hope it still works.

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+const core = require('@actions/core');
 const ical = require("ical-generator");
 const { Octokit } = require("@octokit/rest");
 const MarkdownIt = require("markdown-it");
@@ -10,44 +11,43 @@ function keysToLowercase(obj) {
     return newObj;
 }
 
-console.log('1');
 const cal = ical({
-    domain: 'bitcoindesigners.org',
+    domain: core.getInput('domain'),
     prodId: {
-        company: 'Bitcoin Design Community',
+        company: core.getInput('company'),
         product: 'ical-generator'
     },
-    name: 'Bitcoin Design Community Calls & Events',
-    timezone: 'US/Pacific'
+    name: core.getInput('name'),
+    timezone: core.getInput('timezone')
 });
+
 const octokit = new Octokit({
     //auth: process.env.GITHUB_AUTH,
     useragent: "github-events-calendar v0.1.0"
 });
+
 const md = new MarkdownIt();
-console.log('2');
 const Timezone = "GMT";
-const repositories = [
-    { org: "BitcoinDesign", repo: "Meta", label: "call" },
-    { org: "BitcoinDesign", repo: "Guide", label: "call" },
-    { org: "johnsBeharry", repo: "github-events-calendar", label: "call" },
-];
+
 async function getIssues(owner, repo, label) {
     let request = "GET /repos/:owner/:repo/issues";
     let issues = await octokit.request(request, { owner, repo, labels: label });
     return issues;
 }
-console.log('3');
+
 function getEventObjFromIssue(issue) {
     let markdown = md.parse(issue.body, {});
     let meta = YAML.parse(markdown[0].content);
     meta = keysToLowercase(meta);
-    // check duration
+
+    // Check duration
     meta.durationMs = meta.duration
         ? parseDuration(meta.duration)
         : parseDuration(meta.time);
+
     // Default duration to one hour
     const duration = meta.durationMs ? meta.durationMs : 3600000;
+
     let event = {
         title: issue.title,
         url: issue.url,
@@ -55,6 +55,7 @@ function getEventObjFromIssue(issue) {
         startTime: meta.time,
         duration: duration,
     };
+
     // Create an iCal entry if "utctime" is defined.
     if (meta.utctime) {
         const utcTime = meta.utctime;
@@ -71,21 +72,18 @@ function getEventObjFromIssue(issue) {
         };
         cal.createEvent(eventObject);
     }
-
-    console.log('issue', issue);
-    console.log('meta', meta);
-    console.log('event', event);
     
     return event;
 }
-console.log('4');
-getIssues("BitcoinDesign", "Meta", "call")
-    .then((issues) => {
+
+const repository = core.getInput('repository').split(',');
+
+getIssues(repository[0], repository[1], repository[2]).then((issues) => {
     let events = issues.data.map((issue) => getEventObjFromIssue(issue));
-    cal.save("events.ical", () => {
+    
+    cal.save(core.getInput('file'), () => {
         console.log('something happened', arguments);
     });
-})
-    .catch((err) => {
+}).catch((err) => {
     console.error("error", err);
 });
